3150b722e373d80a512e42b9a94a86237b447ced
wendellnd <wendelldourado.wn@gmail.com>
2025-10-04 13:58:19 -0300

    first commit

---
+++ src/main/java/com/fiap/project_bookstore_mvc/ProjectBookstoreMvcApplication.java
@@ -0,0 +1,13 @@
+package com.fiap.project_bookstore_mvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectBookstoreMvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectBookstoreMvcApplication.class, args);
+	}
+
+}
